3282e442bcbbf75d90dfd2cf668432b1cb3926c2
Elvis Soares <elvissoares1880@gmail.com>
2023-10-18 14:29:31 -0300

    Criado arquivo nao_nomeados

---
+++ aula10.18/nao_nomeados.py
@@ -0,0 +1,12 @@
+# valores nomeados
+a = 1
+b = 1
+soma = a + b
+print(soma)
+
+# valores não nomeados
+print(1 + 1)
+
+# obs: valores não nomeados não se propagam no seu codigo
+
+print(a + 10) # valor nomeado mais valor não nomeado
