c95695e6009f2fa9446a990b227a23852e8a15b8
Elvis Soares <elvissoares1880@gmail.com>
2023-10-18 16:06:38 -0300

    Arquivo desafio02.py atualizado

--- aula10.18/desafio02.py
+++ aula10.18/desafio02.py
@@ -1,6 +1,6 @@
 # Para mudar string faça: Int(nome)
 idade = input("Digite sua idade :")
-idade_de_parente = input("Você pode informar a idade de um parente:")
+idade_de_parente = input("Você pode informar a idade da sua mãe:")
 nome_do_usuario = input("Digite seu nome :")
 print(int(idade) * int(idade_de_parente))
 print(nome_do_usuario)
