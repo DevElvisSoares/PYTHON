fff346dc4d5d159d42e265ab86c66425d99d3f1e
Elvis Soares <elvissoares1880@gmail.com>
2023-10-19 15:32:55 -0300

    O arquivo tipos02.py foi atualizado

--- aula10.19/tipos02.py
+++ aula10.19/tipos02.py
@@ -1,1 +1,14 @@
 # entender tipos e mudanças de tipos dentro do python
+
+a = 50
+b = "50"
+
+print(type(a))
+print(type(b))
+
+idade = int(input("Qual é a sua idade?"))
+print(type(idade))
+
+# uilização do float: moedas, peso, temperatura, medidas, etc. 
+peso = float(input("Qual é o seu peso?"))
+print(type(peso))
