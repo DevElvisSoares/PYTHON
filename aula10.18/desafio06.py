d7ae4a6cc320d6996079cc7a845587a78ebe3b9f
Elvis Soares <elvissoares1880@gmail.com>
2023-10-19 10:20:13 -0300

    A tarefa do arquivo desafio06.py foi concluída

---
+++ aula10.18/desafio06.py
@@ -0,0 +1,4 @@
+base = float(input("Digite o valor da base do quadrado: "))
+altura = float(input("Digite o valor da altura do seu quadrado: "))
+area_do_quadrado = base * altura
+print(area_do_quadrado * 2)
