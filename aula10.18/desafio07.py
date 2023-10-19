261655a103fb1f6c3eb586249d2866083f5c386e
Elvis Soares <elvissoares1880@gmail.com>
2023-10-19 14:34:17 -0300

    O arquivo desafio07.py foi atualizado

--- aula10.18/desafio07.py
+++ aula10.18/desafio07.py
@@ -2,7 +2,7 @@ numero1 = int(input("Digite o primeiro numero inteiro: "))
 nuemero2 = int(input("Digite o segundo numero inteiro: "))
 numero3 = int(input("Digite o terceiro numero inteiro: "))
 
-produto = (2 * numero1) + ((nuemero2 + numero3)/2)
+produto = (2 * numero1) * (nuemero2 /2) + numero3
 print(produto)
 
 soma = ((3 * numero1) + numero3) * nuemero2
