c0fdb2be5b4b7c108e542037a012b5f7932907e9
Elvis Soares <elvissoares1880@gmail.com>
2023-10-20 18:27:46 -0300

    Arquivo desafio08.py foi adicionado

---
+++ aula10.19/desafio08.py
@@ -0,0 +1,3 @@
+fahrenheit = float(input("Digite o valor da temperatura a ser convertido para celsius: "))
+celsius = 5 * ((fahrenheit - 32) / 9)
+print(f'A temperatura é equivalente a {celsius}°celsius')
