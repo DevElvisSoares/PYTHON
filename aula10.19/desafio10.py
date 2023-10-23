2a45eab80e71e18eadf7b05b740425a6d17fcc83
Elvis Soares <elvissoares1880@gmail.com>
2023-10-23 13:31:01 -0300

    A tarefa do arquivo desafio10.py foi concluído

---
+++ aula10.19/desafio10.py
@@ -0,0 +1,6 @@
+# Crie um código que receba o salário atual de um funcionário, que receba o valor em porcentagem de reajuste e calcule o valor do novo salário reajustado de acordo com valor de reajuste(%).
+salario_atual = float(input("Informe o valor do seu salario em reais: "))
+reajuste = float(input("Informe o valor do reajuste em porcentagem: "))
+valor_do_reajuste = salario_atual *  reajuste / 100
+salario_com_reajuste = salario_atual + valor_do_reajuste
+print(f'O do salário com reajuste é R$ {salario_com_reajuste} ')
