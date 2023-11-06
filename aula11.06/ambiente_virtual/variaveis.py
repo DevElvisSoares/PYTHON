b03b9155c5fb12637dfa8558fbfba12158a88f35
Elvis Soares <elvissoares1880@gmail.com>
2023-11-06 16:36:50 -0300

    Arquivo .gitignore alterado e aquivo variaveis.py adicionado

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,12 @@
+# variaveis são usadas para salvar algo na memoria do computador 
+# O sinal de = é utilizado para atribuição de valores
+var01 = "alguma coisa"
+print(var01)
+var01 = "outra coisa"
+print(var01)
+var01= 10
+print(var01)
+var01 = "A" * 10
+print(var01)
+var01 = "A" + "B" + "C"
+print(var01)
