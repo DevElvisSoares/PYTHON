a3c1d501f02651f337b1a66be5199e3345ddcd27
Elvis Soares <elvissoares1880@gmail.com>
2023-11-13 16:28:10 -0300

    Arquivo criado

---
+++ aula11.13/atividade.py
@@ -0,0 +1,7 @@
+lista = ['Eduardo', 'kainan', 'Jeferson Lucas', 'Wesley', 'Alice']
+print(lista)
+lista.append('Francisco')
+del lista[2]
+lista.remove ('Alice')
+lista.append ('Artur')
+print(lista)
