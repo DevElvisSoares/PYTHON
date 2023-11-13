ebc1d96ff6dcfdee443578378dc70981800f2cd2
Elvis Soares <elvissoares1880@gmail.com>
2023-11-13 16:14:24 -0300

    Arquivo criado

---
+++ aula11.13/listas.py
@@ -0,0 +1,30 @@
+# uma lista é representadas por []
+# len - metodo que retona a quantiddade de itens de  uma lista 
+# todo metodo por obrigação retorna uma valor
+'''lista = []
+print (lista, type (lista))
+print (len(lista))
+
+lista = ['front'] # essa forma sobreescreva a lista anterior
+print (lista, type (lista))
+print (len(lista))
+lista = ['back'] # o valor 'back' sobreescreveu o valor de 'front'
+print (lista, type (lista))
+print (len(lista))
+
+lista.append('front') # coloca valores no final da lista
+print(lista, type (lista))'''
+#          0       1      2   3    4
+# reverso -5      -4     -3  -2   -1
+lista = ['back', 'tarde', 21, True, 8.8]
+print(f' A quantidade de alunos na turna é: {lista [2]}')
+lista[2] = 22
+print(lista)
+lista[1] = ['Neyva', 'Alice', 'Geisa', 'Lara'] # lista dentro da lista
+print(lista)
+
+print(lista[-2])
+del lista [-2] # deleta pelo indice
+print(lista[-2])
+
+lista.remove('back')
