a9d7998ef9bf56e704325abb44c5d6af63863cb5
Elvis Soares <elvissoares1880@gmail.com>
2023-11-13 14:51:00 -0300

    Arquivo atualizado

---
+++ aula11.09/ambiente_virtual/atividade02.py
@@ -0,0 +1,19 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(texto[0:5])
+print(texto[6:9])
+print(texto[20:30])
+print(texto[34:44])
+print(texto[54:60])
+print(texto[-10:-1])
+print(texto[-19:-11])
+print(texto[101:110])
+print(texto[114:125])
+print(texto[172:185])
+
+palavra1= texto[0:5]
+palavra2 = texto[-19:-11]
+palavra3 = texto[-10:-1]
+palavra4 = texto[31:33]
+palavra5 = texto[]
+print(palavra1,palavra2,palavra3,palavra4,palavra5)
