a3e7f6195ed9701570883927473c7b06c5e94e3e
Elvis Soares <elvissoares1880@gmail.com>
2023-11-14 15:49:54 -0300

    Arquivo adicionado

---
+++ aula11.14/mutabilidade.py
@@ -0,0 +1,7 @@
+# Alguns cuidados com dados mutaveis
+# Mutaveis - são dados que podem ter seu valor alterado na memoria do dispositivo
+# imutaveis - são dados que só podem ser copiados da memoria do dispositivo
+
+# Na lista o endereço de memoria não muda, mas muda os valores. A strings é o contrario. 
+# o copy cria uma copia em um novo endereço de memoria.
+
