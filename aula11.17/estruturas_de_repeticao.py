1115b52f70157e3228232e14cf4abe4904b876eb
Elvis Soares <elvissoares1880@gmail.com>
2023-11-17 15:51:57 -0300

    arquivo adicionado

---
+++ aula11.17/estruturas_de_repeticao.py
@@ -0,0 +1,13 @@
+# Estruturas de repetição
+# While e o For
+# While - observar a possibilidade de não repetição
+# For - observar a possibilidadde de repetição
+# contador 
+# while condição:
+#   codigo
+#   implementar o contador
+
+# break = quebra as estruturas de repetição
+ 
+# for contador:
+#   codigo
