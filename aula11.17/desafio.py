e9bdbdbc291d9ea34034dca936d6214217b969cd
Elvis Soares <elvissoares1880@gmail.com>
2023-11-20 14:24:35 -0300

    Arquivo atualizado

--- aula11.17/desafio.py
+++ aula11.17/desafio.py
@@ -1,15 +1,15 @@
 # Peça a idade de 20 alunos. Faça um codigo que avisa quanto o aluno tem mais de 13 anos. 
-'''contador = 0
-while contador < 20:
-    idade_aluno = int(input('Informe sua idade: '))
-    if idade_aluno > 13:
-        print(f'O aluno {contador} tem mais de 13 anos')
-    contador += 1'''
+aluno = 1
+while aluno < 20:
+    idade = int(input('Informe a sua idade: '))
+    if idade > 13:
+        print(f' A idade do aluno {aluno} é {idade}. É maior que 13.')
+    aluno += 1
+print('Fim da questão')
 
-
-
+'''
 while True:
     nota = int(input('Digite sua nota: '))
     if (nota < 0 or nota > 10):
-        break
+        break'''
 
